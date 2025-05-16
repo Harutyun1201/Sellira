b63ca167b394002ee7318f6c3699dcb596400431
harutyun_grigoryan <harut@cifora.io>
2025-05-17 02:40:01 +0400

    fix: now tab insert spaces instead of jumping to other UI elements

--- src/editor.js
+++ src/editor.js
@@ -1,6 +1,6 @@
 import { EditorView, highlightActiveLine, keymap, drawSelection } from '@codemirror/view';
 import { EditorState } from '@codemirror/state';
-import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
+import { defaultKeymap, history, historyKeymap, indentWithTab } from '@codemirror/commands';
 import { markdown } from '@codemirror/lang-markdown';
 import { oneDark } from '@codemirror/theme-one-dark';
 
@@ -9,7 +9,11 @@ export function createEditor(parent, content, isDark, onChange) {
     highlightActiveLine(),
     drawSelection(),
     history(),
-    keymap.of([...defaultKeymap, ...historyKeymap]),
+    keymap.of([
+      indentWithTab, // Allow Tab to insert indentation
+      ...defaultKeymap,
+      ...historyKeymap
+    ]),
     markdown(),
     EditorView.updateListener.of((v) => {
       if (v.docChanged) {
